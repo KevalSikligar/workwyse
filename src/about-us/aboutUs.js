8a16247d51b202920c660a8f2be258b6e1d44d65
hda-narola <hda@narola.email>
2020-08-14 17:01:11 +0530

    image changes

--- src/about-us/aboutUs.js
+++ src/about-us/aboutUs.js
@@ -11,27 +11,66 @@ function AboutUs(props) {
         <>
             <BreadCrumbs title="About Us" breadcrumbssegment={pathList} />
             <div>
-                <div class="container">
-                    <div class="section-title center-block text-center">
-                        {/* <h1>About Us</h1> */}
-                        <p>Mauris ut cursus nunc. Morbi eleifend, ligula at consectetur vehicula</p>
+                <div class="sptb bg-white">
+                    <div class="container">
+                        <div class="section-title center-block text-center">
+                            <h1>Our Information</h1>
+                            <p>Mauris ut cursus nunc. Morbi eleifend, ligula at consectetur vehicula</p>
+                        </div>
+                        <div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-4 col-md-4 col-sm-6">
+                                <div class="">
+                                    <div class="mb-lg-0 mb-4">
+                                        <div class="service-card text-center">
+                                            <div class="bg-light icon-bg icon-service text-purple about">
+                                                <img src={require('../assets/images/products/about/megaphone.png')} alt="img" />
+                                            </div>
+                                            <div class="servic-data mt-3">
+                                                <h4 class="font-weight-semibold mb-2">General Account</h4>
+                                                <h5>team@workwyse.io</h5>
+                                                <p class="text-muted mb-0">Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-sm-6">
+                                <div class="">
+                                    <div class="mb-lg-0 mb-4">
+                                        <div class="service-card text-center">
+                                            <div class="bg-light icon-bg icon-service text-purple about">
+                                                <img src={require('../assets/images/products/about/technical.png')} alt="img" />
+                                            </div>
+                                            <div class="servic-data mt-3">
+                                                <h4 class="font-weight-semibold mb-2">Help Technical Issues</h4>
+                                                <h5>support@workwyse.io</h5>
+                                                <p class="text-muted mb-0">Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-4 col-sm-6">
+                                <div class="">
+                                    <div class="mb-lg-0 mb-4">
+                                        <div class="service-card text-center">
+                                            <div class="bg-light icon-bg icon-service text-purple about">
+
+                                                <img src={require('../assets/images/products/about/billing.png')} alt="img" />
+                                            </div>
+                                            <div class="servic-data mt-3">
+                                                <h4 class="font-weight-semibold mb-2">Billing Inquiries</h4>
+                                                <h5>billing@workwyse.io</h5>
+                                                <p class="text-mute mb-0">Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator. </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row">
-                        About
-                        General team@workwyse.io
-                        Born during the Covid crisis of 2020, when offices were left empty with phones left to continuously ring.
-                        Help / Technical Issues support@workwyse.io
-                        Did you ever try cold calling or calling to buy services? I'm betting there were long hold times and voicemail responses.
-                        Billing Enquiries billing@workwyse.io
-                        We set out to create a platform that solves two things; 1. Connect B2B buyers and sellers without having to search through multiple networking & review sites. 2. Qualifies leads for sales teams & doesn't cost an arm and a leg just to connect with them.
-                        Registered Office; company address company address
-                        EC12 XXX
-                        We like to keep things simple and tailored to our users. If you have any ideas on how can improve our service, we'd love to hear from you at team@workwyse.io
-                        Company no. 123456
-                        Vat no. 654 321
-                        Post a Project
-                        Find Buyer
-                </div>
                 </div>
             </div>
         </>
