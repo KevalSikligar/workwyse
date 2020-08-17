07b6d48fcc86e157f1d7322938b465f2204628f7
KPT <kpt@narola.email>
2020-08-17 09:54:50 +0530

    KPT:UI Design Pages

--- src/faq/faq.js
+++ src/faq/faq.js
@@ -1,43 +1,94 @@
 import React from 'react'
 import BreadCrumbs from '../breadcrumbs/breadcrumbs';
+import * as $ from 'jquery';
 
 export default function FAQ() {
 
     const pathList = [
         { to: "/faq", title: "FAQ" }
-    ]
+    ];
 
     return (
         <div>
             <BreadCrumbs title="FAQ" breadcrumbssegment={pathList} />
-            <div class="container">
-                <div id="accordion" class="accordion">
-                    <div class="card mb-0">
-                        <div class="card-header collapsed" data-toggle="collapse" href="#collapseOne">
-                            <a class="card-title">Item 1</a>
-                        </div>
-                        <div id="collapseOne" class="card-body collapse" data-parent="#accordion" >
-                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                            aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat
-                            craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                            </p>
-                        </div>
-                        <div class="card-header collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
-                            <a class="card-title">Item 2</a>
-                        </div>
-                        <div id="collapseTwo" class="card-body collapse" data-parent="#accordion">
-                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                            aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat
-                            craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                            </p>
-                        </div>
-                        <div class="card-header collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
-                            <a class="card-title">Item 3</a>
-                        </div>
-                        <div id="collapseThree" class="collapse" data-parent="#accordion" >
-                            <div class="card-body">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                            aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. samus labore sustainable VHS.
-                        </div>
+            <div class="sptb">
+                <div class="container">
+                    <div class="bs-example">
+                        <div class="accordion" id="accordionExample">
+                            <div class="card">
+                                <div class="card-header" id="headingOne">
+                                    <h2 class="mb-0">
+                                        <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseOne">
+                                            How do Buyers get in touch with Sellers?  <i class="fa fa-plus"></i>
+                                        </button>
+                                    </h2>
+                                </div>
+                                <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                    <div class="card-body">                                        
+                                        <div class="privacy-sub-section">
+                                        <h4>There are two ways for Buyers to initiate contact:</h4>
+                                        <ul>
+                                            <li>Send a 'Work With' request that invites them to your project. You can send requests out to a maximum of 5 Sellers.</li>
+                                            <li>Alternatively, if you decide do not to send any requests. up to 5 Sellers can contact you.</li>
+                                            <li>Send a presonalised message directly to Sellers</li>
+                                            <li>Feel free to share any details once you connect!</li>
+                                        </ul>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header" id="headingTwo">
+                                    <h2 class="mb-0">
+                                        <button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo"> How does it works for sellers? <i class="fa fa-plus"></i></button>
+                                    </h2>
+                                </div>
+                                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                                    <div class="card-body">
+                                        <p>You are matched with Buyers based on your company details. These are set during the sign-up proces. You can also amend your service tags under 'Location & Services' in the settings menu.</p>
+                                        <p>You can send a 'Work with' request to Buyers (limited to x25 a week). if a Buyer responds. you can then converse with them further .</p>
+                                        <p>We only charge for 'Replay to Hire' leads. These cost $5 each with the billing cycle at the end of each week. You can update your billing details under 'Billing' in the Settings menu. (See 'Pricing')</p>
+                                        <p>Leads that are not at the 'Ready to Hire' stage are free to contact. (See 'Pricing')</p>
+                                        <p>You can respond to direct messages sent from Buyers.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header" id="headingThree">
+                                    <h2 class="mb-0">
+                                        <button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree"> How do users communicate? <i class="fa fa-plus"></i></button>
+                                    </h2>
+                                </div>
+                                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                                    <div class="card-body">
+                                        <p>We provide an in-built messaging platform for smooth communication.</p>
+                                        <p>File links can be uploaded cia Google Drive or Dropbox.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header" id="headingFour">
+                                    <h2 class="mb-0">
+                                        <button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour"> What is the (WWI) WorkWyseIndex ratinf and how are profiles rated? <i class="fa fa-plus"></i></button> 
+                                    </h2>
+                                </div>
+                                <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+                                    <div class="card-body">
+                                    <div class="privacy-sub-section">
+                                        <h4> Providing a median of the following scores.</h4>
+                                        <ul class="mb-md-5">                                            
+                                            <li>Glassdoor rating</li>
+                                            <li>Trustpilot rating	</li>
+                                            <li>Google Business rating</li>
+                                            <li>Facebook Page ratting</li>
+                                        </ul>
+                                        </div>
+                                        <p>You can see written reviews by clicking the WWI icon.</p>
+                                        <p>We have our own algorithm that works based on the rating our users provide.</p>
+                                        <p>You never have to worry about reviews made by poorly rated users as they won't affect your score!</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/pricing/pricing.js
+++ src/pricing/pricing.js
@@ -12,7 +12,93 @@ export default class Pricing extends Component {
         return (
             <div>
                 <BreadCrumbs title="Pricing" breadcrumbssegment={pathList} />
-                <h3>
+                <div class="sptb"> 
+                    <div class="container">
+                        <div class="row">
+                        <div class="col-xl-3 col-md-6 col-sm-12 col-lg-3"> 
+                            <div class="pricingTable2 primary"> 
+                                <div class="pricingTable2-header"> 
+                                    <h3>Free</h3> <span>Lorem ipsum dolor</span> 
+                                </div> 
+                                <div class="pricing-plans"> 
+                                    <span class="price-value1"><i class="fa fa-usd"></i><span>0.00</span></span> 
+                                    <span class="month">/month</span> 
+                                </div> 
+                                <div class="pricingContent2"> 
+                                    <ul> 
+                                        <li><b>Free</b> Ad posting</li> 
+                                        <li><b>0</b> Featured Classes</li> 
+                                        <li><b>100%</b> Secure</li> 
+                                        <li><b>Custome</b> Reviews</li> 
+                                        <li><b>24/7</b> Support</li> 
+                                    </ul> 
+                                </div>
+                                <div class="pricingTable2-sign-up"> <a href="#" class="btn btn-block btn-primary">sign up</a> </div> 
+                            </div>                            
+                        </div>
+                         {/* second card */}
+                         <div class="col-xl-3 col-md-6 col-sm-12 col-lg-3"> 
+                                <div class="pricingTable2 orange"> 
+                                    <div class="pricingTable2-header"> <h3>Premium</h3> <span>Lorem ipsum dolor</span> </div> 
+                                    <div class="pricing-plans"> 
+                                        <span class="price-value1"><i class="fa fa-usd"></i><span>19</span></span> 
+                                        <span class="month">/month</span> </div> <div class="pricingContent2"> 
+                                        <ul> 
+                                            <li><b>Featured</b> Ad posting</li> 
+                                            <li><b>20</b> Featured Classes</li> 
+                                            <li><b>100%</b> Secure</li> 
+                                            <li><b>Custome</b> Reviews</li> 
+                                            <li><b>24/7</b> Support</li> 
+                                        </ul> 
+                                    </div>
+                                    <div class="pricingTable2-sign-up"> <a href="#" class="btn btn-block btn-secondary">sign up</a> </div> 
+                                </div> 
+                            </div>
+                        {/* third card */}
+                        <div class="col-xl-3 col-md-6 col-sm-12 col-lg-3"> 
+                            <div class="pricingTable2 green"> 
+                                <div class="pricingTable2-header"> <h3>Silver</h3> <span>Lorem ipsum dolor</span> </div> 
+                                <div class="pricing-plans"> 
+                                    <span class="price-value1"><i class="fa fa-usd"></i><span>67</span></span> 
+                                    <span class="month">/month</span> 
+                                </div> 
+                                <div class="pricingContent2"> 
+                                    <ul> 
+                                        <li><b>Featured</b> Ad posting</li> 
+                                        <li><b>30</b> Featured Classes</li> 
+                                        <li><b>100%</b> Secure</li> <li><b>Custome</b> Reviews</li> 
+                                        <li><b>24/7</b> Support</li> 
+                                    </ul> 
+                                </div> 
+                                <div class="pricingTable2-sign-up"> <a href="#" class="btn btn-block btn-success">sign up</a> </div>
+                            </div> 
+                        </div>
+                        {/* fourth card */}
+                        <div class="col-xl-3 col-md-6 col-sm-12 col-lg-3"> 
+                            <div class="pricingTable2 info"> 
+                                <div class="pricingTable2-header"> <h3>Gold</h3> <span>Lorem ipsum dolor</span> </div> 
+                                <div class="pricing-plans"> 
+                                    <span class="price-value1"><i class="fa fa-usd"></i><span>78</span></span> 
+                                    <span class="month">/month</span> </div> <div class="pricingContent2"> 
+                                    <ul> 
+                                        <li><b>Featured</b> Ad posting</li> 
+                                        <li><b>40</b> Featured Classes</li> 
+                                        <li><b>100%</b> Secure</li> <li><b>Custome</b> Reviews</li> 
+                                        <li><b>24/7</b> Support</li> 
+                                    </ul> 
+                                </div> 
+                                <div class="pricingTable2-sign-up"> <a href="#" class="btn btn-block btn-info">sign up</a> </div>
+                            </div> 
+                        </div>
+                        
+                        
+                        
+                        </div>
+            {/* row */}
+                        
+                    </div>
+                </div>
+                {/* <h3>
                     Pricing
                     We've kept things simple here.
                     Sign Up Completely free, always
@@ -22,7 +108,7 @@ export default class Pricing extends Component {
                     FAQs
                     What customer details do I get? When you respond to a lead, you'll get the customer's email address and can contact them freely via Workwyse's messaging service or via email. Sometimes, leads also add in a contact number
                     Do I get charged per message? No, only the initial 'WorkWith' request message has a cost associated of £5. This is for requests sent to you, or requests that you send;
-                </h3>
+                </h3> */}
             </div>
         )
     }
