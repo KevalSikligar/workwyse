c117f97581c611e4dd2e3e79e0c8354634db269d
Rajvi Khatri <hda@narola.email>
2020-08-18 12:07:32 +0530

    Spelling change and Pricing css added

--- src/about-us/aboutUs.js
+++ src/about-us/aboutUs.js
@@ -35,7 +35,7 @@ function AboutUs(props) {
                                             <div class="servic-data mt-3">
                                                 <h4 class="font-weight-semibold mb-2">General Account</h4>
                                                 <h5>team@workwyse.io</h5>
-                                                <p class="text-muted mb-0">Reference site about Generate Account, giving information on its origins, as well as a random Lipsum generator.</p>
+                                                <p class="text-muted mb-0">Reference site about Generate Account, giving information on its origins, as well as a random acc generator.</p>
                                             </div>
                                         </div>
                                     </div>
@@ -51,7 +51,7 @@ function AboutUs(props) {
                                             <div class="servic-data mt-3">
                                                 <h4 class="font-weight-semibold mb-2">Help Technical Issues</h4>
                                                 <h5>support@workwyse.io</h5>
-                                                <p class="text-muted mb-0">Reference site about Technical Issues, giving information on its origins, as well as a random Lipsum generator.</p>
+                                                <p class="text-muted mb-0">Reference site about Technical Issues, giving information on its origins, as well as a random technical generator.</p>
                                             </div>
                                         </div>
                                     </div>
@@ -67,7 +67,7 @@ function AboutUs(props) {
                                             <div class="servic-data mt-3">
                                                 <h4 class="font-weight-semibold mb-2">Billing Inquiries</h4>
                                                 <h5>billing@workwyse.io</h5>
-                                                <p class="text-mute mb-0">Reference site about Billing Inquiries, giving information on its origins, as well as a random Lipsum generator. </p>
+                                                <p class="text-mute mb-0">Reference site about Billing Inquiries, giving information on its origins, as well as a random billing psum generator. </p>
                                             </div>
                                         </div>
                                     </div>
@@ -76,43 +76,43 @@ function AboutUs(props) {
                         </div>
                     </div>
                 </div>
-                <div class="cover-image sptb bg-background-color contact-bg"> 
-                    <div class="content-text mb-0"> 
-                        <div class="container"> 
+                <div class="cover-image sptb bg-background-color contact-bg">
+                    <div class="content-text mb-0">
+                        <div class="container">
                             <div class="text-center text-white section-title pb-7">
                                 <h1>Workwyse is a registered association in Luzern, Switzerland </h1>
                             </div>
                             <div class="row">
                                 <div class="col-lg-6 col-md-6 col-sm-12 col-12 bg-white p-0">
                                     <div class="about-map-section">
-                                          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238133.1523816246!2d72.68221020433099!3d21.15914250210564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04e59411d1563%3A0xfe4558290938b042!2sSurat%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1597424132065!5m2!1sen!2sin"  frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-                                       </div>
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238133.1523816246!2d72.68221020433099!3d21.15914250210564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04e59411d1563%3A0xfe4558290938b042!2sSurat%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1597424132065!5m2!1sen!2sin" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                                    </div>
                                 </div>
                                 <div class="col-lg-6 col-md-6 col-sm-12 col-12 contact-us-desc">
                                     <h4 class="contact-us-title text-white"> Contact Details</h4>
                                     <div class="filedlist">
-                                    <div class="contact-us-static-fields">
-                                        <h4><i class="fa fa-address-card"></i>Workwyse Association</h4>
-                                        <hr class="mb-40" />
-                                    </div>
-                                    <div class="contact-us-static-fields">
-                                        <h4><i class="fa fa-phone"></i>+91 8866339158</h4>
-                                        <hr class="mb-40" />
-                                    </div>
-                                    <div class="contact-us-static-fields">
-                                        <h4><i class="fa fa-calculator"></i>654321</h4>
-                                        <hr class="mb-40" />
-                                    </div>
-                                    <div class="contact-us-static-fields">
-                                        <h4><i class="fa fa-map-marker"></i>Horw, 5058 Switzerland</h4>
-                                        <hr class="mb-40" />
-                                    </div>
-                                    <div class="mt-5"> <a href="#" class="btn btn-contact-us">Contact Us</a> </div>
+                                        <div class="contact-us-static-fields">
+                                            <h4><i class="fa fa-address-card"></i>Workwyse Association</h4>
+                                            <hr class="mb-40" />
+                                        </div>
+                                        <div class="contact-us-static-fields">
+                                            <h4><i class="fa fa-phone"></i>+91 8866339158</h4>
+                                            <hr class="mb-40" />
+                                        </div>
+                                        <div class="contact-us-static-fields">
+                                            <h4><i class="fa fa-calculator"></i>654321</h4>
+                                            <hr class="mb-40" />
+                                        </div>
+                                        <div class="contact-us-static-fields">
+                                            <h4><i class="fa fa-map-marker"></i>Horw, 5058 Switzerland</h4>
+                                            <hr class="mb-40" />
+                                        </div>
+                                        <div class="mt-5"> <a href="#" class="btn btn-contact-us">Contact Us</a> </div>
                                     </div>
                                 </div>
                             </div>
-                        </div> 
-                    </div> 
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
--- src/privacy/privacy.js
+++ src/privacy/privacy.js
@@ -103,7 +103,7 @@ export default function Privacy() {
                 </li>
               </ul>
             </div>
-            <div className='privacy-sub-section mt-md-5'>            
+            <div className='privacy-sub-section mt-md-5'>
               <h4>
                 {' '}
                 In this privacy policy, unless the context requires a different
@@ -142,103 +142,102 @@ export default function Privacy() {
               </ul>
             </div>
             <div className='privacy-sub-section mt-md-5'>
-            <h3> Scope of this privacy policy</h3>
-            <p>
-              {' '}
+              <h3> Scope of this privacy policy</h3>
+              <p>
+                {' '}
               This privacy policy applies only to the actions of WorkWyse Ltd and Users with respect to this Website. It does not extend to any websites that can be accessed from this Website including, but not limited to, any links we may provide to social media websites.
             </p>
-            <p>
-              {' '}
+              <p>
+                {' '}
               For purposes of the applicable Data Protection Laws, WorkWyse Ltd is the "data controller". This means that WorkWyse Ltd determines the purposes for which, and the manner in which, your Data is processed.
-            </p>              
+            </p>
             </div>
             <div className='privacy-sub-section mt-md-5'>
-            <h3> Data collected</h3>
-            <h4> We may collect the following Data, which includes personal Data, from you:</h4>
-            <ul>
-              <li>Name;</li>
-              <li>Job title;</li>
-              <li>Profession;</li>
-              <li>Contact Information such as email addresses and telephone numbers;</li>
-              <li>Demographic information such as postcode, preferences and interests;</li>
-              <li>Financial information such as credit / debit card numbers;</li>
-              <li>IP address (automatically collected);</li>
-              <li>Web browser type and version (automatically collected);</li>
-              <li>ompany number, vat number, email address, phone number, location, additional company and user information required as per sign up process;</li>
-              
-            </ul>
-            <p class="mt-md-4">
-              {' '}
+              <h3> Data collected</h3>
+              <h4> We may collect the following Data, which includes personal Data, from you:</h4>
+              <ul>
+                <li>Name;</li>
+                <li>Job title;</li>
+                <li>Profession;</li>
+                <li>Contact Information such as email addresses and telephone numbers;</li>
+                <li>Demographic information such as postcode, preferences and interests;</li>
+                <li>Financial information such as credit / debit card numbers;</li>
+                <li>IP address (automatically collected);</li>
+                <li>Web browser type and version (automatically collected);</li>
+                <li>Company number, vat number, email address, phone number, location, additional company and user information required as per sign up process;</li>
+              </ul>
+              <p class="mt-md-4">
+                {' '}
               in each case, in accordance with this privacy policy.
             </p>
-                         
-            </div> 
+
+            </div>
             <div className='privacy-sub-section mt-md-5'>
               <h3>How we collect Data</h3>
               <h4>We collect Data in the following ways:</h4>
               <ul>
                 <li>Data is given to us by you ; </li>
                 <li>Data is received from other sources; and</li>
-                <li>Data is collected automatically.</li>              
-              </ul>        
-            </div>         
-            
+                <li>Data is collected automatically.</li>
+              </ul>
+            </div>
+
             <div className='privacy-sub-section mt-md-5'>
               <h3>Data that is given to us by you</h3>
               <h4>WorkWyse Ltd will collect your Data in a number of ways, for example:</h4>
               <ul>
                 <li>When you contact us through the Website, by telephone, post, e-mail or through any other means; </li>
                 <li>When you register with us and set up an account to receive our products/services;</li>
-                <li>When you complete surveys that we use for research purposes (although you are not obliged to respond to them);</li>              
+                <li>When you complete surveys that we use for research purposes (although you are not obliged to respond to them);</li>
                 <li>When you enter a competition or promotion through a social media channel;</li>
                 <li>When you make payments to us, through this Website or otherwise;</li>
                 <li>When you elect to receive marketing communications from us;</li>
-                <li>When you use our services;</li>                
-              </ul>        
+                <li>When you use our services;</li>
+              </ul>
               <p class="mt-md-4">
-              {' '}
+                {' '}
               in each case, in accordance with this privacy policy.
             </p>
-            </div>  
+            </div>
             <div className='privacy-sub-section mt-md-5'>
               <h3>Data that is received from third parties</h3>
               <h4>WorkWyse Ltd will receive Data about you from the following third parties:</h4>
               <ul>
-                <li>LinkedIn. </li>                
-              </ul>                      
-            </div>           
+                <li>LinkedIn. </li>
+              </ul>
+            </div>
             <div className='privacy-sub-section mt-md-5'>
               <h3>Data that is received from publicly available third parties sources</h3>
               <h4>We will receive Data about you from the following publicly available third party sources:</h4>
               <ul>
-                <li>Trustpilot, Glassdoor, Google, Facebook.</li>                
-              </ul>                      
+                <li>Trustpilot, Glassdoor, Google, Facebook.</li>
+              </ul>
             </div>
             <div className='privacy-sub-section mt-md-5'>
               <h3>Data that is collected automatically</h3>
               <h4>To the extent that you access the Website, we will collect your Data automatically, for example:</h4>
               <ul>
-                <li>We automatically collect some information about your visit to the Website. This information helps us to make improvements to Website content and navigation, and includes your IP address, the date, times and frequency with which you access the Website and the way you use and interact with its content.</li>                
+                <li>We automatically collect some information about your visit to the Website. This information helps us to make improvements to Website content and navigation, and includes your IP address, the date, times and frequency with which you access the Website and the way you use and interact with its content.</li>
                 <li>We will collect your Data automatically via cookies, in line with the cookie settings on your browser. For more information about cookies, and how we use them on the Website, see the section below, headed "Cookies".</li>
-              </ul>                      
+              </ul>
             </div>
             <div className='privacy-sub-section mt-md-5'>
               <h3>Our use of Data</h3>
               <h4>Any or all of the above Data may be required by us from time to time in order to provide you with the best possible service and experience when using our Website. Specifically, Data may be used by us for the following reasons:</h4>
               <ul>
-                <li>Internal record keeping;</li>                
+                <li>Internal record keeping;</li>
                 <li>Improvement of our products / services;</li>
                 <li>Transmission by email of marketing materials that may be of interest to you;</li>
                 <li>Contact for market research purposes which may be done using email, telephone, fax or mail. Such information may be used to customise or update the Website;</li>
-              </ul> 
-              <p class="mt-md-4">             
-              {' '}
+              </ul>
+              <p class="mt-md-4">
+                {' '}
               in each case, in accordance with this privacy policy.
-            </p>    
-            <p>
-              {' '}
+            </p>
+              <p>
+                {' '}
               We may use your Data for the above purposes if we deem it necessary to do so for our legitimate interests. If you are not satisfied with this, you have the right to object in certain circumstances (see the section headed "Your rights" below).
-            </p>                     
+            </p>
             </div>
 
 
@@ -271,9 +270,9 @@ export default function Privacy() {
             <p>If any court or competent authority finds that any provision of this privacy policy (or part of any provision) is invalid, illegal or unenforceable, that provision or part-provision will, to the extent required, be deemed to be deleted, and the validity and enforceability of the other provisions of this privacy policy will not be affected.</p>
             <p>Unless otherwise agreed, no delay, act or omission by a party in exercising any right or remedy will be deemed a waiver of that, or any other, right or remedy.</p>
             <p>This Agreement will be governed by and interpreted according to the law of England and Wales. All disputes arising under the Agreement will be subject to the exclusive jurisdiction of the English and Welsh courts.</p>
-            <h3>Changes to this privacy policy</h3>        
+            <h3>Changes to this privacy policy</h3>
             <p>WorkWyse Ltd reserves the right to change this privacy policy as we may deem necessary from time to time or as may be required by law. Any changes will be immediately posted on the Website and you are deemed to have accepted the terms of the privacy policy on your first use of the Website following the alterations.   You may contact WorkWyse Ltd by email at team@workwyse.io.</p>
-            
+
             {/* 
                             <p>You are responsible for safeguarding the password that you use to access the Services and you agree not to disclose your password to any third party You are responsible for any activity using your account, whether or not you authorized that activity. You should immediately notify Workwyse Ltd of any unauthorized use of your account.</p>
                             <h3>Your General Responsibilities</h3>
